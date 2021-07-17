93c550bd88544c15fbbc7c33059f441a8a77c784
Jason Schalk <jasonschalk@yahoo.com>
2021-07-16 23:03:13 -0400

    block reward calibration Since not all blocks in the past had the maximum reward in the coinbase transaction therefore start at a recent block instead of block 1 added an option: -b <Block Number> that displays data for a specific block height

--- block_reward/src/main.cpp
+++ block_reward/src/main.cpp
@@ -6,36 +6,84 @@
 #include "dummy_params.h"
 #include "GetBlockSubsidy.snip"
 
-int main()
+typedef uint32_t BlockID;
+
+void display_help(void);
+void display_block_info(BlockID block_height, CAmount reward, CAmount total_reward, bool indicator);
+
+int main(int argc, char *argv[])
 {
 	Consensus::Params defaults;
 	CAmount total_reward = 0;
 	bool over_cap = false;
-	int block_height;
-	
+	BlockID block_height;
+	BlockID block_info_height = 0;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "hb:")) != -1) {
+		switch (opt) {
+			case 'b':	// displays data for a specified block #
+				block_info_height = strtoul(optarg, NULL, 10);
+				break;
+
+			default:
+			case 'h':
+				display_help();
+				exit(0);
+		}
+	}
+
 	printf("block_height, reward, total_reward\n");
-	
-	for(block_height = 1; block_height < 42602000; block_height++)
+// 	output of digibyte-cli gettxoutsetinfo                                                              
+// 	{
+// 		"height": 13307241,
+// 		"bestblock": "d9d02169bfa210c3785b8641e0e297ab80dca88bba9b4942f58f3cc95c87a159",
+// 		"transactions": 3076170,
+// 		"txouts": 10470013,
+// 		"bogosize": 791114141,
+// 		"hash_serialized_2": "ae4f5a33b0b7c8d9ccf548d48e1af539f9bdaa0f13e332f55af19ab1df7f85d6",
+// 		"disk_size": 566378280,
+// 		"total_amount": 14495186190.33644281
+// 	}
+// calibrated using block 13307241 because not all blocks specified the maximum block reward
+	total_reward = 1449518619033644281;
+// start @ 13307242 because the block reward for 13307241 is already included in total_reward
+	for(block_height = 13307242; block_height < 42802000; block_height++)
 	{
-		static CAmount last_reward = 0;
 		CAmount reward = GetBlockSubsidy(block_height, defaults);
 		total_reward += reward;
-		if(!over_cap)
+
+		if(block_height == block_info_height)
+			display_block_info(block_height, reward, total_reward, true);
+		if(!over_cap && (total_reward >= (21000000000 * COIN)))
 		{
-			if(total_reward >= (21000000000 * COIN))
-			{
-				over_cap = true;
-				time_t final_block_time = (block_height - 13168400) * 15 + 1624402422;	// calibrated off of block 13168400 with 15 second block times
-				struct tm *my_tm = gmtime(&final_block_time);
-				char text[48];
-				strftime(text, sizeof(text), "%F", my_tm);
-				printf("%d,%ld.%08ld,%ld.%08ld, on, %s\n", block_height, reward / COIN, reward % COIN, total_reward / COIN, total_reward % COIN, text);
-			}
+			over_cap = true;
+			display_block_info(block_height, reward, total_reward, true);
 		}
+
+		static CAmount last_reward = 0;
 		if(reward != last_reward)
 		{
-			printf("%d,%ld.%08ld,%ld.%08ld\n", block_height, reward / COIN, reward % COIN, total_reward / COIN, total_reward % COIN);
+			display_block_info(block_height, reward, total_reward, false);
 			last_reward = reward;
 		}
 	}
+	return 0;
+}
+
+void display_help(void)
+{
+	printf("block_reward:\n");
+	printf(" -b <Block Number>  outputs data for the given block number\n");
+	printf(" -h                 help\n");
+}
+
+void display_block_info(BlockID block_height, CAmount reward, CAmount total_reward, bool indicator)
+{
+	time_t final_block_time = (block_height - 13168400) * 15 + 1624402422;	// calibrated off of block 13168400 with 15 second block times
+	char text[48];
+	struct tm *my_tm = gmtime(&final_block_time);
+	strftime(text, sizeof(text), "%F", my_tm);
+
+	printf("%d,%ld.%08ld,%ld.%08ld,%s%s\n", block_height, reward / COIN, reward % COIN, total_reward / COIN, total_reward % COIN, text, indicator ? ",<---" : "");
 }
